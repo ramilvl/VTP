d8d8e854d6d30b66f9242e2e24469091629f29bf
Ramil Valiyev <ramil4360616@gmail.com>
2022-12-23 00:18:34 +0400

    Java file committed

---
+++ src/main/java/ActionAyarlar.java
@@ -0,0 +1,10 @@
+
+import javax.swing.JFrame;
+
+public class ActionAyarlar {
+    public static void setVisible(JFrame old, JFrame neww)
+    {
+        old.setVisible(false);
+        neww.setVisible(true);
+    }
+}
